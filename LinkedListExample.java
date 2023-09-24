fb633698f5d715de1dc60c55a5f2c0758196520f
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-09-24 18:58:24 +0800

    Create LinkedListExample.java

---
+++ LinkedListExample.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class LinkedListExample {
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> linky = new LinkedList<Integer>();
+		
+		/*
+		//LinkedList as a Stack
+		linky.push(1);
+		linky.push(2);
+		linky.push(3);
+		linky.pop();
+
+		//LinkedList as a Queue
+		linky.offer(1);
+		linky.offer(2);
+		linky.offer(4);
+		//linky.poll();
+
+		linky.add(2, 3);
+		linky.remove(3);
+		System.out.println(linky.indexOf(4));
+		System.out.println(linky.peekFirst());
+		System.out.println(linky.peekLast());
+		linky.addFirst(0);
+		linky.addLast(5);
+		int first = linky.removeFirst();
+		int last = linky.removeLast();
+		
+		System.out.print(linky);
+
+		Iterator it = linky.iterator();
+		while(it.hasNext()) {
+			if((int) it.next() == 4) {
+				System.out.print("Found it!");
+			}
+		} */
+	}
+}
